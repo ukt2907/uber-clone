99e6ec1a13560b1a724d253859969c3abeca86b0
Umesh Thakur <thakur.sonunet.sonu@gmail.com>
2025-04-08 23:59:04 +0530

    added

--- frontend/src/components/ConfirmRide.tsx
+++ frontend/src/components/ConfirmRide.tsx
@@ -16,7 +16,7 @@ const ConfirmRide = ({...props}: ConfirmRideProps) => {
   return (
     <div className="flex rounded-xl flex-col gap-5">
         <div className="w-full flex justify-center">
-        <img className="size-42" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png" alt="" />
+        <img className="size-40" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png" alt="" />
         </div>
         <h1 className="text-xl font-semibold mb-5 text-center">Confirm Ride</h1>
         <div className="flex   flex-col   px-4">
